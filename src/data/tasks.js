20b64fe05e8856dee772eb8a6c72230f99f00171
Ro1984Bi <bilbaorodrigojavi@outlook.com>
2022-12-12 13:23:05 -0300

    react project

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "first task",
+    description: "first description",
+  },
+  {
+    id: 1,
+    title: "second task",
+    description: "second description",
+  },
+  {
+    id: 2,
+    title: "third task",
+    description: "third description",
+  },
+];
